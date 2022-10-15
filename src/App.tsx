025c323134abd5c526e55f7a3b23ea83ac8c908a
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2022-10-15 18:02:37 +0600

    added pagination but create a bug from sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,75 @@
-import React, { useState } from 'react';
-import data from './data.json';
+import React, { useState } from "react";
+// import data from "./data.json";
 
-import Table from './table/Table';
+import Table, { Column } from "./table/Table";
 
 function App() {
+    const columns: Column[] = [
+        {
+            title: "Image",
+            dataIndex: "coverPhoto",
+            render: (i: string) => <img className="w-10" src="img_avatar5.png" alt="" />,
+        },
+        {
+            title: "Title",
+            dataIndex: "title",
+            sorter: (a: string, b: string) => {
+                return a > b ? 1 : a < b ? -1 : 0;
+            },
+        },
+        {
+            title: "brandId",
+            dataIndex: "brandId",
+        },
+        {
+            title: "categoryId",
+            dataIndex: "categoryId",
+        },
+        {
+            title: "Price",
+            dataIndex: "price",
+            sorter: (a: number, b: number) => {
+                return a > b ? 1 : a < b ? -1 : 0;
+            },
+        },
+        {
+            title: "Stock",
+            dataIndex: "qty",
+        },
+        {
+            title: "Sold",
+            dataIndex: "sold",
+        },
+    ];
 
-  const columns = [
-    {
-      title: 'Image',
-      dataIndex: 'coverPhoto',
-      render: (i: string) => <img src={i} alt="" />,
-    },
-    {
-      title: 'Title',
-      dataIndex: 'title',
-      sorter: (a: any, b: any) => {
-        return a > b ? 1 : a < b ? -1 : 0;
-      },
-    },
-    {
-      title: 'brandId',
-      dataIndex: 'brandId',
-    },
-    {
-      title: 'categoryId',
-      dataIndex: 'categoryId',
-    },
-    {
-      title: 'Price',
-      dataIndex: 'price',
-      sorter: (a: any, b: any) => {
-        return a > b ? 1 : a < b ? -1 : 0;
-      },
-    },
-    {
-      title: 'Stock',
-      dataIndex: 'qty',
-    },
-    {
-      title: 'Sold',
-      dataIndex: 'sold',
-    },
-  ];
-  
+    const data = new Array(1000).fill(1).map((_, index) => ({
+        title: "Lenovo IdeaPad 3 Core i3 10th Gen -- " + index,
+        qty: index,
+        sold: index,
+        categoryId: "60df5e546419f56b97610607",
+        price: 410 + index,
+        brandId: "613511bba937c621233341c8",
+        createdAt: "2011-08-04T11:55:16.887Z",
+        coverPhoto: "",
+    }));
 
-  return (
-    <div className="App p-10">
-      <h1 className="text-3xl my-6 text-center font-bold">Responsive Table</h1>
-      <div className="mt-10">
-        
-        <div className="card">
-        <Table
-            className=""
-            fixed={true}
-            dataSource={data ? data : []}
-            scroll={{x: 800, y: 350}}
-            columns={columns}
-        />
-      </div>
-      
-      </div>
-    </div>
-  );
+    return (
+        <div className="App p-4 md:p10">
+            <h1 className="text-3xl my-6 text-center font-bold">Responsive Table</h1>
+            <div className="mt-10">
+                <div className="card">
+                    <Table
+                        className=""
+                        fixed={true}
+                        dataSource={data ? data : []}
+                        scroll={{ x: 800, y: 450 }}
+                        columns={columns}
+                        pagination={{ pageSize: 10, currentPage: 1 }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/table/Table.tsx
+++ src/table/Table.tsx
@@ -5,105 +5,153 @@ import Tbody from "./Tbody";
 import "./style.css";
 
 export interface Column extends HTMLAttributes<HTMLElement> {
-  id?: string;
-  title: string;
-  dataIndex?: string;
-  className?: string;
-  colWidth?: number;
-  sorter?: (args1: any, args2: any) => any;
-  render?: (key: string) => React.ReactNode;
+    id?: string;
+    title: string;
+    dataIndex?: string;
+    className?: string;
+    colWidth?: number;
+    sorter?: (args1: any, args2: any) => any;
+    render?: (key: string) => React.ReactNode;
 }
 [];
 
 interface TableProps extends HTMLAttributes<HTMLTableElement> {
-  dataSource: any;
-  theadClass?: { th?: string; td?: string; thead?: string };
-  tbodyClass?: { tr?: string; td?: string; tbody?: string };
-  columns: Column[];
-  scroll?: { x?: number; y?: number };
-  fixed?: boolean;
+    dataSource: any;
+    theadClass?: { th?: string; td?: string; thead?: string };
+    tbodyClass?: { tr?: string; td?: string; tbody?: string };
+    columns: Column[];
+    scroll?: { x?: number; y?: number };
+    fixed?: boolean;
+    pagination?: { pageSize: number; currentPage: number };
 }
 
 const Table: FC<TableProps> = (props) => {
-  const {
-    theadClass,
-    tbodyClass,
-    className,
-    dataSource,
-    columns,
-    fixed,
-    scroll,
-  } = props;
-  let fixedTable = { maxHeight: 0, minWidth: 0 };
+    const {
+        theadClass,
+        tbodyClass,
+        className,
+        dataSource,
+        columns,
+        fixed,
+        scroll,
+        pagination = { pageSize: 100, currentPage: 1 },
+    } = props;
 
-  const [state, setState] = useState({
-    items: [],
-    order: 1,
-    sortedField: "",
-  });
+    let fixedTable = { maxHeight: 0, minWidth: 0 };
 
-  useEffect(() => {
-    setState((prevState) => ({
-      ...prevState,
-      items: dataSource,
-      order: 1,
-    }));
-  }, []);
+    const [state, setState] = useState({
+        items: [],
+        paginatedItems: [],
+        order: 1,
+        sortedField: "",
+        currentPage: 1,
+        pageSize: 100,
+    });
 
-  if (scroll && scroll.y) {
-    fixedTable.maxHeight = scroll.y;
-  }
-  if (scroll && scroll.x) {
-    fixedTable.minWidth = scroll.x;
-  }
+    useEffect(() => {
+        setState((prevState) => {
+            let list = dataSource;
 
-  function handleSort(
-    compareFn: (args1: any, args2: any) => void,
-    column: any
-  ) {
-    let list = dataSource;
-    if (state.order) {
-      list = list.sort((a: any, b: any) =>
-        compareFn(a[column.dataIndex], b[column.dataIndex])
-      );
-    } else {
-      list = list.sort((a: any, b: any) =>
-        compareFn(b[column.dataIndex], a[column.dataIndex])
-      );
+            if (pagination) {
+                list = sliceForPaginate(
+                    {
+                        currentPage: pagination.currentPage,
+                        pageSize: pagination.pageSize,
+                    },
+                    list
+                );
+            }
+
+            return {
+                ...prevState,
+                items: dataSource,
+                paginatedItems: list,
+                order: 1,
+                currentPage: pagination.currentPage,
+                pageSize: pagination.pageSize,
+            };
+        });
+    }, []);
+
+    if (scroll && scroll.y) {
+        fixedTable.maxHeight = scroll.y;
+    }
+    if (scroll && scroll.x) {
+        fixedTable.minWidth = scroll.x;
     }
 
-    setState((prevState) => ({
-      ...prevState,
-      items: list,
-      sortedField: column.dataIndex,
-      order: prevState.order ? 0 : 1,
-    }));
-  }
+    function handleSort(compareFn: (args1: any, args2: any) => void, column: any) {
+        let list = dataSource;
+        if (state.order) {
+            list = list.sort((a: any, b: any) => compareFn(a[column.dataIndex], b[column.dataIndex]));
+        } else {
+            list = list.sort((a: any, b: any) => compareFn(b[column.dataIndex], a[column.dataIndex]));
+        }
 
-  return (
-    <div
-      className={`rsl-table ${fixed ? "table-fixed" : ""}`}
-      style={fixedTable.maxHeight ? { maxHeight: fixedTable.maxHeight } : {}}
-    >
-      <table
-        className={className}
-        style={fixedTable.minWidth ? { minWidth: fixedTable.minWidth } : {}}
-      >
-        <Thead
-          onSort={handleSort}
-          theadClass={theadClass}
-          columns={columns}
-          sortedField={state.sortedField}
-          order={state.order}
-        />
-        <Tbody
-          tbodyClass={tbodyClass}
-          dataSource={state.items}
-          columns={columns}
-        />
-      </table>
-    </div>
-  );
+        setState((prevState) => ({
+            ...prevState,
+            items: list,
+            sortedField: column.dataIndex,
+            order: prevState.order ? 0 : 1,
+        }));
+    }
+
+    function sliceForPaginate(state: any, list: any) {
+        return list.slice((state.currentPage - 1) * state.pageSize, state.pageSize * state.currentPage);
+    }
+
+    function handleSelectPage(pageNumber: number) {
+        let list = sliceForPaginate(
+            {
+                currentPage: pageNumber,
+                pageSize: state.pageSize,
+            },
+            state.items
+        );
+
+        setState((prevState) => ({
+            ...prevState,
+            currentPage: pageNumber,
+            paginatedItems: list,
+        }));
+    }
+
+    return (
+        <div>
+            <div
+                className={`rsl-table ${fixed ? "table-fixed" : ""}`}
+                style={fixedTable.maxHeight ? { maxHeight: fixedTable.maxHeight } : {}}
+            >
+                <table className={className} style={fixedTable.minWidth ? { minWidth: fixedTable.minWidth } : {}}>
+                    <Thead
+                        onSort={handleSort}
+                        theadClass={theadClass}
+                        columns={columns}
+                        sortedField={state.sortedField}
+                        order={state.order}
+                    />
+                    <Tbody
+                        tbodyClass={tbodyClass}
+                        dataSource={pagination ? state.paginatedItems : state.items}
+                        columns={columns}
+                    />
+                </table>
+            </div>
+
+            <div className="flex items-center flex-wrap ">
+                {state.items.slice(0, state.items.length / state.pageSize).map((_, index) => (
+                    <div
+                        className={`bg-gray-100 rounded-full font-medium m-2 w-12 h-12 flex justify-center text-center items-center cursor-pointer ${
+                            state.currentPage === index + 1 ? "!bg-amber-400" : ""
+                        }`}
+                        onClick={() => handleSelectPage(index + 1)}
+                    >
+                        <button>{index + 1}</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Table;
